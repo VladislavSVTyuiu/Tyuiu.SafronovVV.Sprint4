a3694bc8e9b5d4afc946af293df77dd921cad3a7
X-Ray2003 <134064048+X-Ray2003@users.noreply.github.com>
2023-11-02 20:22:52 +0500

    Task3

---
+++ Tyuiu.SafronovVV.Sprint4.Task3.V17.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SafronovVV.Sprint4.Task3.V17.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SafronovVV.Sprint4.Task3.V17.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5122ae6a-aff3-4d28-9e3a-c559f3d0b13c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
