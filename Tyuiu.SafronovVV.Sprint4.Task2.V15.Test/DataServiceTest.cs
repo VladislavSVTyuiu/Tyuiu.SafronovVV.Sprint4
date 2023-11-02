006849f6b985d33438459c3f0ff1703b91533be2
X-Ray2003 <134064048+X-Ray2003@users.noreply.github.com>
2023-11-02 20:10:40 +0500

    Task2

---
+++ Tyuiu.SafronovVV.Sprint4.Task2.V15.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.SafronovVV.Sprint4.Task2.V15.Lib;
+
+namespace Tyuiu.SafronovVV.Sprint4.Task2.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] array = { 2, 6, 5, 5, 1, 5, 6, 5, 5, 5, 1 };
+            Assert.AreEqual(14, ds.Calculate(array));
+        }
+    }
+}
---
+++ Tyuiu.SafronovVV.Sprint4.Task2.V15.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SafronovVV.Sprint4.Task2.V15.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SafronovVV.Sprint4.Task2.V15.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("508462d1-7511-4570-a448-15e0e5275c46")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
